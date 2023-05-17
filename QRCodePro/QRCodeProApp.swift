df94123932dd3bb529e3392cc2ac137fe71f41e2
Quang Bao <hoangquangbao.developer@gmail.com>
2023-05-18 06:45:49 +0700

    Created basic UI

---
+++ QRCodePro/QRCodeProApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRCodeProApp.swift
+//  QRCodePro
+//
+//  Created by Quang Bao on 17/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct QRCodeProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
