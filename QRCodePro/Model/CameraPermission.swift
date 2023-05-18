77599803b445374e714ae9b2db1439e5ee8643c9
Quang Bao <hoangquangbao.developer@gmail.com>
2023-05-18 13:14:08 +0700

    Present error access permission message

---
+++ QRCodePro/Model/CameraPermission.swift
@@ -0,0 +1,16 @@
+//
+//  CameraPermission.swift
+//  QRCodePro
+//
+//  Created by Quang Bao on 18/05/2023.
+//
+
+import Foundation
+import SwiftUI
+
+enum CameraPermission: String {
+    case notDetermined = "Not Determined"
+    case restricted = "Restricted"
+    case denied = "Access dined"
+    case authorized = "Acess Granted"
+}
